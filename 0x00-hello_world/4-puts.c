bdd53e33199d48da9d5985f2b4ad15ff7a79b28f
wiizy123 <wiizyhimsev@gmail.com>
2023-02-16 18:06:41 -0800

    Welcome to c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program that prints string in puts function
+ * Return: Always return 0.
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+/**
+ * main - prints with proper grammer
+ *
+ * Return: always return 0.
+ */
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			/* always return 0*/
+			return (0);
+}
