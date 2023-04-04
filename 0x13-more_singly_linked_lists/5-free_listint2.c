f0b1b498168187e0ab8a1281615c29834aec29ae
Bana Isaiah <83048838+wiizy123@users.noreply.github.com>
2023-04-04 15:30:59 -0800

    update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,18 +1,19 @@
 #include "lists.h"
 
+
 void free_listint2(listint_t **head)
 {
-	listint_t *temp_node;
+ listint_t *temp;
 
-	if (head == NULL)
-		return;
+ if (head == NULL)
+ return;
 
-	while (*head != NULL)
-	{
-		temp_node = (*head)->next;
-		free(*head);
-		*head = temp_node;
-	}
+ while (*head)
+ {
+ temp = (*head)->next;
+ free(*head);
+ *head = temp;
+ }
 
-	*head = NULL;
+ *head = NULL;
 }
