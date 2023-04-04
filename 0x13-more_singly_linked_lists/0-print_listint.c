cd3ccc0f0cc47486147e00703782e6ca4e3db525
Bana Isaiah <83048838+wiizy123@users.noreply.github.com>
2023-04-04 15:22:14 -0800

    update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,16 +1,16 @@
+
 #include "lists.h"
-#include <stdio.h>
 
 size_t print_listint(const listint_t *h)
 {
-	size_t set = 0;
+ size_t num = 0;
 
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		set++;
-		h = h->next;
-	}
+ while (h)
+ {
+ printf("%d\n", h->n);
+ num++;
+ h = h->next;
+ }
 
-	return (set);
+ return (num);
 }
