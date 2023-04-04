61b9cb42849d4af27bcb216cb47fea387cd06a29
Bana Isaiah <83048838+wiizy123@users.noreply.github.com>
2023-04-04 15:34:19 -0800

    update 8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,21 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
+
 
 int sum_listint(listint_t *head)
 {
-	int sum;
-	listint_t *next_ptr;
+ int sum = 0;
+ listint_t *temp = head;
 
-	if (!head)
-		return (0);
-	next_ptr = head;
-	sum = 0;
-	while (next_ptr != NULL)
-	{
-		sum += next_ptr->n;
-		next_ptr = next_ptr->next;
-	}
-	return (sum);
+ while (temp)
+ {
+ sum += temp->n;
+ temp = temp->next;
+ }
+
+ return (sum);
 }
+
