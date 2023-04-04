463a7aa9936c3ca1acd547cfd7ac42cbd722d60d
Bana Isaiah <83048838+wiizy123@users.noreply.github.com>
2023-04-04 15:33:00 -0800

    update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,19 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i;
-	listint_t *next_ptr;
+ unsigned int i = 0;
+ listint_t *temp = head;
 
-	next_ptr = head;
-	for (i = 0; i < index; i += 1)
-	{
-		if (next_ptr == NULL)
-			return (NULL);
-		next_ptr = next_ptr->next;
-	}
-	return (next_ptr);
+ while (temp && i < index)
+ {
+ temp = temp->next;
+ i++;
+ }
+
+ return (temp ? temp : NULL);
 }
