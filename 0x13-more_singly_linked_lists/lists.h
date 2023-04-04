44b73de439dc288c66e6f190eab1e1364baa5482
Bana Isaiah <83048838+wiizy123@users.noreply.github.com>
2023-04-04 15:20:52 -0800

    update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,18 +1,16 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-
-
+#include <stdio.h>
 #include <stdlib.h>
 
 
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+ int n;
+ struct listint_s *next;
 } listint_t;
 
-int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -30,3 +28,4 @@ size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
 #endif
+/*LISTS_H*/
