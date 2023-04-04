498758b224a3655ac58c374b4b4ce96ea022eb50
Bana Isaiah <83048838+wiizy123@users.noreply.github.com>
2023-04-04 10:43:04 -0800

    update mylibrary.h

--- 0x13-more_singly_linked_lists/mylibrary.h
+++ 0x13-more_singly_linked_lists/mylibrary.h
@@ -1,31 +1,29 @@
-#ifndef MYLIBRARY_H
-#define MYLIBRARY_H
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdio.h>
 #include <stdlib.h>
 
-
-typedef struct node_s
+typedef struct listint_s
 {
     int value;
-    struct node_s *next;
-} node_t;
+    struct listint_s *next;
+} listint_t;
 
-size_t print_node(const node_t *h);
-size_t node_len(const node_t *h);
-node_t *add_node(node_t **head, const int value);
-node_t *add_node_end(node_t **head, const int value);
-void free_node(node_t *head);
-void free_node2(node_t **head);
-int pop_node(node_t **head);
-node_t *get_node_at_index(node_t *head, unsigned int index);
-int sum_node(node_t *head);
-node_t *insert_node_at_index(node_t **head, unsigned int idx, int value);
-int delete_node_at_index(node_t **head, unsigned int index);
-node_t *reverse_node(node_t **head);
-size_t print_node_safe(const node_t *head);
-size_t free_node_safe(node_t **h);
-node_t *find_node_loop(node_t *head);
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int value);
+listint_t *add_nodeint_end(listint_t **head, const int value);
+void free_listint(listint_t *head);
+void free_listint2(listint_t **head);
+int pop_listint(listint_t **head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int sum_listint(listint_t *head);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int value);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 
-#endif /* MYLIBRARY_H */
-
+#endif
