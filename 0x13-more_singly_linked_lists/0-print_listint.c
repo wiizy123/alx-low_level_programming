f1565a599ee3cb401c659f503efef2fdd619fcf1
Bana Isaiah <83048838+wiizy123@users.noreply.github.com>
2023-04-04 13:21:22 -0800

    update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t counter = 0;
+	size_t num = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		counter++;
+		num++;
 		h = h->next;
 	}
 
-	return (counter);
+	return (num);
 }
