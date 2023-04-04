3d5f56bfaa72444314b6bd6402cf6201da51b9e0
Bana Isaiah <83048838+wiizy123@users.noreply.github.com>
2023-04-04 15:25:25 -0800

    update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,13 +3,13 @@
 
 size_t listint_len(const listint_t *h)
 {
-	size_t set = 0;
+ size_t num = 0;
 
-	while (h != NULL)
-	{
-		set++;
-		h = h->next;
-	}
+ while (h)
+ {
+ num++;
+ h = h->next;
+ }
 
-	return (set);
+ return (num);
 }
