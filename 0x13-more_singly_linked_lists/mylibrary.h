076e391a81fb431ddc5675b4f714e3608cdcfcc2
Bana Isaiah <83048838+wiizy123@users.noreply.github.com>
2023-04-04 11:27:37 -0800

    Update and rename lists.h to mylibrary.h

---
+++ 0x13-more_singly_linked_lists/mylibrary.h
@@ -0,0 +1,41 @@
+#ifndef MYLIBRARY_H
+#define MYLIBRARY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+int _putchar(char c);
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+void free_listint2(listint_t **head);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int pop_listint(listint_t **head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int sum_listint(listint_t *head);
+
+
+#endif
