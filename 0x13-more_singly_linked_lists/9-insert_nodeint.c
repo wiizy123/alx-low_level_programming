307b24c35a19852f43865cb2662f5f9e82da3cb5
Bana Isaiah <83048838+wiizy123@users.noreply.github.com>
2023-04-04 15:35:28 -0800

    update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,36 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
+
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *next_ptr;
-	listint_t *previous_ptr;
-	listint_t *new_node;
-	unsigned int i;
+ unsigned int i;
+ listint_t *new;
+ listint_t *temp = *head;
 
-	if (head == NULL)
-		return (NULL);
-	next_ptr = *head;
-	previous_ptr = NULL;
-	for (i = 0; i < idx; i += 1)
-	{
-		if (next_ptr == NULL)
-			return (NULL);
-		previous_ptr = next_ptr;
-		next_ptr = next_ptr->next;
-	}
+ new = malloc(sizeof(listint_t));
+ if (!new || !head)
+ return (NULL);
 
-	new_node = malloc(sizeof(listint_t));
-	if (!new_node)
-		return (NULL);
-	new_node->n = n;
-	new_node->next = next_ptr;
-	if (idx == 0)
-		*head = new_node;
-	else
-		previous_ptr->next = new_node;
+ new->n = n;
+ new->next = NULL;
 
-	return (new_node);
+ if (idx == 0)
+ {
+ new->next = *head;
+ *head = new;
+ return (new);
+ }
+
+ for (i = 0; temp && i < idx; i++)
+ {
+ if (i == idx - 1)
+ {
+ new->next = temp->next;
+ temp->next = new;
+ return (new);
+ }
+ else
+ temp = temp->next;
+ }
+
+ return (NULL);
 }
