879877888808902a36ffd8a4ae17ee7604f2e764
Bana Isaiah <83048838+wiizy123@users.noreply.github.com>
2023-04-04 18:09:25 -0800

    update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,16 @@
 #include "lists.h"
 
-/**
- * print_listint - prints all the elements of a linked list
- * @h: linked list of type listint_t to print
- *
- * Return: number of nodes
- */
+
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t vim = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		num++;
+		vim++;
 		h = h->next;
 	}
 
-	return (num);
+	return (vim);
 }
