3dee15d7f07e08e69dfc8ba109278b6a4b2f6ae1
Bana Isaiah <83048838+wiizy123@users.noreply.github.com>
2023-04-10 11:30:23 -0800

    update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,8 +14,7 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t bytes_read, bytes_written;
+    int fd, bytes_read, bytes_written;
     char *buffer;
 
     if (filename == NULL)
