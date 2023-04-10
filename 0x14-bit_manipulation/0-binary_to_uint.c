f9a6a1304770d55b038fc1fe5abfdd57c691ca80
Bana Isaiah <83048838+wiizy123@users.noreply.github.com>
2023-04-10 12:45:57 -0800

    update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,19 +11,18 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int num = 0;
+	unsigned int num = 0;
 
-    if (b == NULL)
-        return 0;
+	if (b == NULL)
+		return (0);
 
-    for (; *b; b++)
-    {
-        if (*b != '0' && *b != '1')
-            return 0;
+	for (int i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
 
-        num = num * 2 + (*b - '0');
-    }
+		num = (num * 2) + (b[i] - '0');
+	}
 
-    return num;
+	return (num);
 }
-
